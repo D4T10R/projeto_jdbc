6a5219a01fef2515541acbc5469cf06977e95f50
Datior <leonardowederveiga@yahoo.com>
2023-01-02 13:36:38 -0300

    SellerDAO e DaoFactory criados

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.Seller;
+import model.dao.SellerDAO;
+
+public class SellerDaoJDBC implements SellerDAO {
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obt) {
+        
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+    
+}
--- src/principal/Program.java
+++ src/principal/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import model.Department;
 import model.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 
 public class Program {
     
@@ -14,6 +16,8 @@ public class Program {
 
         Seller seller = new Seller(21, "Bob", "bobo@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDao();
 
     }
 
